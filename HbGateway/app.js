8f62704243a43a86da22c1872d136c0253a75e4d
4QL-NB10\Mike <mike.robertson@4ql.co.uk>
2016-10-07 06:57:06 +0100

    Connected to Local CouchDb

--- HbGateway/app.js
+++ HbGateway/app.js
@@ -1,5 +1,10 @@
 ﻿var PouchDb = require("pouchdb");
-var db = new PouchDb("hameboss1");
+var db = new PouchDb("http://localhost:5984/hameboss1", {
+    auth: {
+        username: 'admin',
+        password: 'brilt'
+    }
+});
 
 db.info().then(function(info){
     console.log(info);
